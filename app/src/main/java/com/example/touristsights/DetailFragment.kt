e8ea11ccf1b71c76d7194ee09591de5b9e5fd8b1
shacyno <shacyno@gmail.com>
2021-06-24 12:50:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/touristsights/DetailFragment.kt
@@ -0,0 +1,43 @@
+package com.example.touristsights
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.touristsights.databinding.FragmentDetailBinding
+
+const val ROW_POSITION = "ROW_POSITION"
+
+class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private var position: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            position = it.getInt(ROW_POSITION)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sights = getSights(resources)
+        binding.detailKind.text = sights[position].kind
+        binding.detailName.text = sights[position].name
+        binding.detailDescription.text = sights[position].description
+        var img = resources.getIdentifier(sights[position].imageName,
+                                          "drawable", context?.packageName)
+        binding.detailImage.setImageResource(img)
+    }
+}
---
+++ app/src/main/java/com/example/touristsights/ListFragment.kt
@@ -0,0 +1,67 @@
+package com.example.touristsights
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.touristsights.databinding.FragmentListBinding
+
+class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.apply {
+            layoutManager =
+                when {
+                    resources.configuration.orientation
+                            == Configuration.ORIENTATION_PORTRAIT
+                    -> LinearLayoutManager(context)
+                    else
+                    -> GridLayoutManager(context, 2)
+                }
+            adapter = SightAdapter(context, getSights(resources)).apply {
+                setOnItemClickListener {position: Int ->
+                    fragmentManager?.let {manager: FragmentManager ->
+                        val tag = "DetailFragment"
+                        var fragment = manager.findFragmentByTag(tag)
+                        if (fragment == null) {
+                            fragment = DetailFragment()
+                            fragment.arguments = Bundle().apply {
+                                putInt(ROW_POSITION, position)
+                            }
+                            manager.beginTransaction().apply {
+                                setCustomAnimations(
+                                    android.R.anim.slide_in_left,
+                                    android.R.anim.slide_out_right,
+                                    android.R.anim.fade_in,
+                                    android.R.anim.fade_out)
+                                replace(R.id.content, fragment, tag)
+                                addToBackStack(null)
+                            }.commit()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
